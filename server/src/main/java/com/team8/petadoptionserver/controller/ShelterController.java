23fd9e9320668d6deb897d02b32264ffebba9d64
gabriel <gabramirez.17@gmail.com>
2023-11-14 20:11:25 -0800

    Create ShelterController.java
    added ShelterController

---
+++ server/src/main/java/com/team8/petadoptionserver/controller/ShelterController.java
@@ -0,0 +1,47 @@
+package com.team8.petadoptionserver.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.team8.petadoptionserver.dao.ShelterDAO;
+import com.team8.petadoptionserver.model.Shelter;
+
+@RestController
+@RequestMapping("/api/shelter")
+public class ShelterController {
+
+    public final ShelterDAO shelterDAO;
+
+    @Autowired
+    public ShelterController(ShelterDAO shelterDAO) {
+        this.shelterDAO = shelterDAO;
+    }
+
+    @PostMapping({ "", "/" })
+    public int addShelter(@RequestBody Shelter shelter) {
+        return shelterDAO.addShelter(shelter);
+    }
+
+    @GetMapping({ "", "/" })
+    public List<Shelter> findAll(){
+        return shelterDAO.findAll();
+    }
+
+    @GetMapping("/id/{id}")
+    public Optional<Shelter> findById(@PathVariable ("id") int id) {
+        return shelterDAO.findById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public int updateShelter(RequestBody Shelter shelter, @PathVariable("id") int id) {
+        return shelterDAO.updateShelter(id, shelter);
+    }
+
+    @GetMapping("/delet/{id}")
+    public int deleteShelter(@PathVariable("id") int id) {
+        shelterDAO.deleteShelter(id);
+    }
+}
