b3e837e2bf93e2d33dee7951bae33733787fc5c1
alex <alexnbui@gmail.com>
2023-11-29 03:49:54 -0800

    Refactor content display

--- client/src/reusable/Content/Content.tsx
+++ client/src/reusable/Content/Content.tsx
@@ -5,15 +5,6 @@ import { useQuery } from "@tanstack/react-query"
 
 type ContentView = "card" | "list"
 type PaneView = "customers" | "volunteers" | "pets" | "shelters"
-type DataEntry = {
-    id: number,
-    name: string,
-    sex: "M" | "F"
-    age: number,
-    breed: string,
-    desc: string,
-    img: string,
-}
 
 type Props = {
     contentView: ContentView,
@@ -73,16 +64,37 @@ export default function Content({ contentView, paneView }: Props) {
             return axios.get(`http://localhost:5000/${paneView}/name/${name}`)
         }
     })
-    const data: Array<DataEntry> = query.data?.data ?? defaultCards
+    const data: Array<any> = query.data?.data ?? defaultCards
+    // EVERYBODY PANIC THERE IS NO TYPE SAFETY AHHHHHHHHHH
+
+    const cardFactory = (element: any) => {
+        switch (paneView) {
+            case "pets":
+                return <Card key={element.id} name={element.name} summary={`${element.sex} - ${element.age > 12 ? `${Math.floor(element.age / 12)}yr` : `${element.age}mo`} - ${element.breed}`} desc={element.desc} img={element.img} />
+            case "volunteers":
+            case "customers":
+            case "shelters":
+        }
+    }
+
+    const listElementFactory = (element: any) => {
+        switch (paneView) {
+            case "pets":
+                return <ListElement key={element.id} name={element.name} summary={`${element.sex} - ${element.age > 12 ? `${Math.floor(element.age / 12)}yr` : `${element.age}mo`} - ${element.breed}`} desc={element.desc} img={element.img} />
+            case "volunteers":
+            case "customers":
+            case "shelters":
+        }
+    }
 
     return (
         <>
             {contentView == "card" ?
                 <div style={{ gridTemplateColumns: "repeat(auto-fill, minmax(20rem, 1fr))", gridAutoRows: "13rem" }} className="grid gap-8 h-[calc(100%-4rem)] overflow-y-auto p-6">
-                    {data.map((element) => <Card key={element.id} name={element.name} summary={`${element.sex} - ${element.age > 12 ? `${Math.floor(element.age / 12)}yr` : `${element.age}mo`} - ${element.breed}`} desc={element.desc} img={element.img} />)}
+                    {data.map((element) => cardFactory(element))}
                 </div>
                 : <div className="h-[calc(100%-4rem)] overflow-y-auto px-6 py-2">
-                    {data.map((element) => <ListElement key={element.id} name={element.name} summary={`${element.sex} - ${element.age > 12 ? `${Math.floor(element.age / 12)}yr` : `${element.age}mo`} - ${element.breed}`} desc={element.desc} img={element.img} />)}
+                    {data.map((element) => listElementFactory(element))}
                 </div>
             }
         </>
