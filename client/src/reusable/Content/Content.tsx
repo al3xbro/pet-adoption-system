b5ab1e5df38a59f552592bed84389d1199efe160
alex <alexnbui@gmail.com>
2023-12-01 05:23:35 -0800

    Connect both ends

--- client/src/reusable/Content/Content.tsx
+++ client/src/reusable/Content/Content.tsx
@@ -2,6 +2,8 @@ import axios from "axios"
 import Card from "./Card"
 import ListElement from "./ListElement"
 import { useQuery } from "@tanstack/react-query"
+import { useSearchParams } from "react-router-dom"
+import { useEffect } from "react"
 
 type ContentView = "card" | "list"
 type PaneView = "customers" | "volunteers" | "pets" | "shelters"
@@ -11,61 +13,25 @@ type Props = {
     paneView: PaneView
 }
 
-const defaultCards = [
-    {
-        id: 1,
-        name: "Fluffyfasdfasdfasdfasdfadfasd",
-        sex: "M",
-        age: 24,
-        breed: "Poodle",
-        desc: "Fluffy is a happy dog who loves to play fetch",
-        img: "https://ih1.redbubble.net/image.2653429219.4814/st,small,507x507-pad,600x600,f8f8f8.webp"
-    },
-    {
-        id: 2,
-        name: "Mittensfasdfasdafasdfasdfas",
-        sex: "F",
-        age: 36,
-        breed: "Tabby",
-        desc: "Mittens is a happy cat who loves to play with yarn",
-        img: "https://ih1.redbubble.net/image.2653429219.4814/st,small,507x507-pad,600x600,f8f8f8.webp"
-
-    },
-    {
-        id: 3,
-        name: "Bubbles",
-        sex: "M",
-        age: 12,
-        breed: "Goldfish",
-        desc: "Bubbles is a happy fish who loves to swim",
-        img: "https://ih1.redbubble.net/image.2653429219.4814/st,small,507x507-pad,600x600,f8f8f8.webp"
-
-    },
-    {
-        id: 4,
-        name: "Croc",
-        sex: "F",
-        age: 36,
-        breed: "Crocodile",
-        desc: "Croc is a happy crocodile who loves to eat",
-        img: "https://ih1.redbubble.net/image.2653429219.4814/st,small,507x507-pad,600x600,f8f8f8.webp"
-
-    }
-]
-
 export default function Content({ contentView, paneView }: Props) {
 
-    const { searchParams } = new URL(window.location.href)
-    const name = searchParams.get("q")
+    let [searchParams] = useSearchParams();
+    let name = searchParams.get("q");
+
     // FIXME: endpoint
     const query = useQuery({
-        queryKey: ["pet", name],
+        queryKey: [paneView, name],
         queryFn: async () => {
-            return axios.get(`http://localhost:5000/api/${paneView}/name/${name}`)
+            if (!name) return axios.get(`http://localhost:8080/api/${paneView}`)
+            else return axios.get(`http://localhost:8080/api/${paneView}/name/${name}`)
         }
     })
-    const data: Array<any> = query.data?.data ?? defaultCards
     // EVERYBODY PANIC THERE IS NO TYPE SAFETY AHHHHHHHHHH
+
+    useEffect(() => {
+        console.log(searchParams.get("q"))
+        query.refetch()
+    }, [searchParams])
 
     // FIXME: json data
     const cardFactory = (element: any) => {
@@ -96,13 +62,16 @@ export default function Content({ contentView, paneView }: Props) {
 
     return (
         <>
-            {contentView == "card" ?
-                <div style={{ gridTemplateColumns: "repeat(auto-fill, minmax(20rem, 1fr))", gridAutoRows: "13rem" }} className="grid gap-8 h-[calc(100%-4rem)] overflow-y-auto p-6">
-                    {data.map((element) => cardFactory(element))}
-                </div>
-                : <div className="h-[calc(100%-4rem)] overflow-y-auto px-6 py-2">
-                    {data.map((element) => listElementFactory(element))}
-                </div>
+            {
+                query.isSuccess ?
+                    contentView == "card" ?
+                        <div style={{ gridTemplateColumns: "repeat(auto-fill, minmax(20rem, 1fr))", gridAutoRows: "13rem" }} className="grid gap-8 h-[calc(100%-4rem)] overflow-y-auto p-6">
+                            {query.data?.data.map((element: any) => cardFactory(element))}
+                        </div>
+                        : <div className="h-[calc(100%-4rem)] overflow-y-auto px-6 py-2">
+                            {query.data?.data.map((element: any) => listElementFactory(element))}
+                        </div>
+                    : null
             }
         </>
     )
---
+++ server/src/main/java/com/team8/petadoptionserver/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.team8.petadoptionserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // Allow all origins
+        config.addAllowedMethod("*"); // Allow all methods (GET, POST, etc.)
+        config.addAllowedHeader("*"); // Allow all headers
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
