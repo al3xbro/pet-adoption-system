e6dd9fc784d8d5db6cd993fec4458527928d78aa
Madison Kolley <53013984+Meowitsmadi@users.noreply.github.com>
2023-12-11 22:59:59 -0800

    fixing mapping tag

--- server/src/main/java/com/team8/petadoptionserver/controller/AdoptsController.java
+++ server/src/main/java/com/team8/petadoptionserver/controller/AdoptsController.java
@@ -29,7 +29,7 @@ public class AdoptsController {
         return adoptsDAO.addAdoption(adoptionData);
     }
 
-    @GetMapping({ "", "/" })
+    @GetMapping("/shelter/{petId}")
     public List<Map<String, Object>> findAvailablePets() {
         return adoptsDAO.getAvailablePets();
     }
